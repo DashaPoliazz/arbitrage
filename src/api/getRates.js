ee0365f164abf58e55387e7eb2e858405c52e3bf
Pasha <dasha.poliazzz@gmail.com>
2024-08-01 00:01:01 +0200

    response messages has been formatted

--- src/api/getRates.js
+++ src/api/getRates.js
@@ -25,54 +25,49 @@
    * ```
    *
    * @param {Object} params - The parameters for retrieving the rates.
-   * @param {string} params.inputCurrency - The ticker symbol of the input currency.
-   * @param {string} params.outputCurrency - The ticker symbol of the output currency.
+   * @param {string} params.baseCurrency - The ticker symbol of the input currency.
+   * @param {string} params.quoteCurrency - The ticker symbol of the output currency.
    * @returns {Promise<Array<{symbol: string, price: number, stockExchange: string}>>} A promise that resolves to an array of objects representing the rates from different exchanges.
    */
-
-  async getRates({ inputCurrency, outputCurrency }) {
+  async getRates({ baseCurrency, quoteCurrency }) {
     const prices = [];
     for (const client of clients) {
-      const price = await client.getPrice(inputCurrency, outputCurrency);
+      const price = await client.getPrice(baseCurrency, quoteCurrency);
 
       // We have to consider few cases:
       // 1. If pair "I-O" exists, and we can just add it to prices
       if (!price.isError) {
-        prices.push({ ...price, stockExchange: client.name });
+        prices.push({ exchangeName: client.name, rate: price.price });
         continue;
       }
       // 2. If it doesn't
       //    Maybe reversed ratio exists "O-I"
-      const reversedPair = await client.getPrice(outputCurrency, inputCurrency);
+      const reversedPair = await client.getPrice(quoteCurrency, baseCurrency);
       if (!reversedPair.isError) {
         const price = reversedPair.price;
         // Since the ratio is revert, we have to revert it again to get direct pair
         const normalizedPrice = 1 / price;
         prices.push({
-          isError: false,
-          symbol: `${inputCurrency}${outputCurrency}`,
-          price: normalizedPrice,
-          stockExchange: client.name,
+          exchangeName: client.name,
+          rate: normalizedPrice,
         });
         continue;
       }
 
       //    We will try to derive the price from "INPUT-USDT" & "OUTPUT-USDT"
-      const inputUsdtPair = await client.getPrice(inputCurrency, "USDT");
-      //    if we can't obtain inputUsdtPair, then it's impossible to derive pair "InputCurrencty/OutputCurrency"
+      const inputUsdtPair = await client.getPrice(baseCurrency, "USDT");
+      //    if we can't obtain inputUsdtPair, then it's impossible to derive pair "InputCurrencty/quoteCurrency"
       if (inputUsdtPair.isError) continue;
       //    otherwise, we can try to fetch "OUTPUT-USDT"
-      const outputUsdtPair = await client.getPrice(outputCurrency, "USDT");
-      //    if we can't obtain outputUsdtPair, then it's impossible to derive pair "InputCurrencty/OutputCurrency"
+      const outputUsdtPair = await client.getPrice(quoteCurrency, "USDT");
+      //    if we can't obtain outputUsdtPair, then it's impossible to derive pair "InputCurrencty/quoteCurrency"
       if (outputUsdtPair.isError) continue;
 
       const derivedPrice = inputUsdtPair.price / outputUsdtPair.price;
       //    checking whether it's the lowest ask across all stock exchanges
       prices.push({
-        isError: false,
-        symbol: `${inputCurrency}${outputCurrency}`,
-        price: derivedPrice,
-        stockExchange: client.name,
+        exchangeName: client.name,
+        rate: derivedPrice,
       });
     }
     return prices;
